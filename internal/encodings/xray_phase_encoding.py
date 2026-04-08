e1d3a7b3263304b6adedd648d44c952e4a35b091
shi_jie <1592277253@qq.com>
2026-04-08 14:54:49 +0800

    调整 PhaseEncoding 类的构造函数，修改 T 参数类型为 float，移除 DC 分量(t)输出

--- internal/encodings/xray_phase_encoding.py
+++ internal/encodings/xray_phase_encoding.py
@@ -9,20 +9,24 @@ class PhaseEncoding(nn.Module):
     def __init__(
         self, 
         input_channels: int = 1, 
-        T: int = 1,
+        T: float = 1.,
         n_frequencies: int = 1,
     ):
         r"""
-        Defines a function that embeds periodic t with Period T to (t, sin(2\pi f / T t), cos(2\pi f / T t), ...), f = [1, 2, ..., n_frequencies]
-        in_channels: number of input channels (3 for both xyz and direction)
-        T: period of the phase
-        n_frequencies: number of frequencies to use. 
+        Defines a function that embeds periodic t with Period T to (sin(2\pi f / T t), cos(2\pi f / T t), ...), 
+        f = [1, 2, ..., n_frequencies]. Attention, there is no DC component (t) in the output, which is different 
+        from the original positional encoding in NeRF.
+        
+        Args:
+            in_channels: number of input channels (3 for both xyz and direction)
+            T: period of the phase
+            n_frequencies: number of frequencies to use. 
         """
         super().__init__()
         self.input_channels = input_channels
         self.T = T
         self.n_frequencies = n_frequencies
-        self.output_channels = input_channels * (2 * n_frequencies + 1)
+        self.output_channels = input_channels * 2 * n_frequencies
 
         max_frequencies = n_frequencies
         freq_bands = 2 ** torch.linspace(0, max_frequencies, steps=n_frequencies)
@@ -33,7 +37,7 @@ class PhaseEncoding(nn.Module):
     
     def forward(self, t: torch.Tensor):
         r"""
-        Embeds t to (t, sin(2\pi f / T t), cos(2\pi f / T t), ...)
+        Embeds t to (sin(2\pi f / T t), cos(2\pi f / T t), ...)
         f = [1, 2, ..., self.n_frequencies]
         
         Inputs:
@@ -48,7 +52,7 @@ class PhaseEncoding(nn.Module):
         cos_res = torch.cos(angles)
         sincos_res = torch.stack([sin_res, cos_res], dim=-1) # (B, C, n_frequencies, 2)
         sincos_res = einops.rearrange(sincos_res, 'B C F D -> B (C F D)')
-        return torch.cat([t, sincos_res], dim=-1)
+        return sincos_res
     
     def get_output_n_channels(self) -> int:
         return self.output_channels
