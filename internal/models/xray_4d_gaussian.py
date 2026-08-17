fix: rotation of C-arm now follows dicom protcol; ATTENTION: must use new generated data, otherwise the rotation will be wrong.

internal/models/xray_4d_gaussian.py
@@ -349,7 +349,7 @@ class Xray4DGaussianModel(
         # Initialize the GS size to be the average dist of the 3 nearest neighbors
         dist2_avg = (knn(fused_point_cloud, 4)[:, 1:] ** 2).mean(dim=-1)  # [N,]
         dist_avg = torch.sqrt(dist2_avg)
-        scales = torch.log(dist_avg * init_scale).unsqueeze(-1).repeat(1, 3)  # [N, 3]
+        scales = self.scale_inverse_activation(dist_avg * init_scale).unsqueeze(-1).repeat(1, 3)  # [N, 3]
 
         n_gaussians = fused_point_cloud.shape[0]
         inits = GaussianInits(n_gaussians, density_res_freq_max=self.config.optimization.density_freq_res_max)